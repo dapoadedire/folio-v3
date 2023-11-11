757f2da55c619d5eb54c91d184da2c5a3dc0c0e1
Dapo Adedire <adedireadedapo19@gmail.com>
2023-11-11 15:15:04 +0100

    add comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dapoadedire.vercel.app',
+  site: 'https://dapoadedire-v3.vercel.app',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });

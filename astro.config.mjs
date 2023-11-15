97c2afffc8510472547f219d609548da16dbb527
Dapo Adedire <adedireadedapo19@gmail.com>
2023-11-15 11:24:16 +0100

    feat: Add Vercel serverless adapter
    This commit adds the Vercel serverless adapter to the Astro
    configuration file. The Vercel adapter enables serverless deployments on
    Vercel and also includes web analytics support. The necessary
    dependencies for Vercel integration are also added to the package.json
    file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
+import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dapoadedire.vercel.app',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });

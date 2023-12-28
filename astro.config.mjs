08078717a21427530c18656610632b00a86a3d74
Dapo Adedire <adedireadedapo19@gmail.com>
2023-12-28 18:56:25 +0100

    Revert "feat: Add Vercel serverless adapter"
    This reverts commit 97c2afffc8510472547f219d609548da16dbb527.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dapoadedire.vercel.app',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  integrations: [mdx(), sitemap(), tailwind(), react()]
 });
